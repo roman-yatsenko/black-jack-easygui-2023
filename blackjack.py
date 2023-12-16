baf23cc9278e6b965cd6af87fb927da2f41b6b3f
YaRo <roman.yatsenko@hneu.net>
2023-12-16 15:08:41 +0200

    Add game icon

--- blackjack.py
+++ blackjack.py
@@ -8,6 +8,7 @@ import games
 
 
 TITLE = "Блек Джек"
+ICON = "black-jack.png"
 
 
 class BJ_Card(cards.Positionable_Card):
@@ -190,7 +191,7 @@ class BJ_Game:
 
 
 def main():
-    gui.msgbox("Ласкаво просимо до гри Блек-джек!", TITLE)
+    gui.msgbox("Ласкаво просимо до гри Блек-джек!", TITLE, image=ICON)
 
     names = []
     number = games.ask_number("Скільки всього гравців? (1 - 7): ", low=1, high=7)
