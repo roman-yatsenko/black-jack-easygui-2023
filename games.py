d9e690519bdd51519ac42e37ef54b4145e2fdaed
YaRo <roman.yatsenko@hneu.net>
2023-12-16 13:56:46 +0200

    Convert games module

--- games.py
+++ games.py
@@ -1,26 +1,26 @@
 # Модуль games
 # Демонструє створення модуля
 
-def ask_yes_no(question):
+import easygui as gui
+
+
+def ask_yes_no(question, title="Game"):
     """Задає питання з відповіддю (y/n)."""
-    response = None
-    while response not in ("y", "n"):
-        response = input(question + ' (y/n)? ').lower()
-    return response
+    return gui.ynbox(question + "?", title)
 
-def ask_number(question, low, high):
+
+def ask_number(question, low, high, title="Game"):
     """Просить ввести число із заданого діапазону."""
-    response = None
-    while response not in range(low, high + 1):
-        response = int(input(question))
-    return response
+    return gui.integerbox(
+        question, title, default=int((low + high) / 2), lowerbound=low, upperbound=high
+    )
 
-  
+
 if __name__ == "__main__":
-    print("Ви запустили модуль games, "
-          "а не імпортували його (import games).")
-    print("Тестування модуля.")
+    gui.msgbox("Ви запустили модуль games, " "а не імпортували його (import games).")
+    gui.msgbox("Тестування модуля.")
     answer = ask_yes_no("Продовжуємо тестування")
-    print("Функція ask_yes_no повернула", answer)
+    print(answer)
+    gui.msgbox("Функція ask_yes_no повернула " + str(answer))
     answer = ask_number("Введіть ціле число від 1 до 10:", 1, 10)
-    print("Функція ask_number повернула", answer)
+    gui.msgbox("Функція ask_number повернула " + str(answer))
